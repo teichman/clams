3c48f975beca6cb3a34bd65116456edf3148f044
Alex Teichman <teichman@cs.stanford.edu>
2013-08-07 17:08:32 -0700

    Adding stub of sseq_to_freiburg program.  Depth and color images are being saved properly.  Just need to add some metadata files and it's done.

---
+++ ros-pkg/core/stream_sequence/src/prog/sseq_to_freiburg.cpp
@@ -0,0 +1,71 @@
+#include <stream_sequence/stream_sequence_base.h>
+#include <iomanip>
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
+
+
+using namespace std;
+namespace bfs = boost::filesystem;
+using namespace clams;
+
+int main(int argc, char** argv)
+{
+  namespace bpo = boost::program_options;
+  bpo::options_description opts_desc("Allowed options");
+  bpo::positional_options_description p;
+
+  string src;
+  string dst;
+  opts_desc.add_options()
+    ("help,h", "produce help message")
+    ("src", bpo::value(&src)->required(), "StreamSequence, i.e. asus data")
+    ("dst", bpo::value(&dst)->required(), "Output path for Freiburg-format dataset")
+    ;
+
+  p.add("src", 1);
+  p.add("dst", 1);
+
+  bpo::variables_map opts;
+  bpo::store(bpo::command_line_parser(argc, argv).options(opts_desc).positional(p).run(), opts);
+  bool badargs = false;
+  try { bpo::notify(opts); }
+  catch(...) { badargs = true; }
+  if(opts.count("help") || badargs) {
+    cout << "Usage: " << argv[0] << " [OPTS] SRC DST" << endl;
+    cout << endl;
+    cout << opts_desc << endl;
+    return 1;
+  }
+
+  ROS_ASSERT(!bfs::exists(dst));
+  StreamSequenceBase::Ptr sseq = StreamSequenceBase::initializeFromDirectory(src);
+  bfs::create_directory(dst);
+  bfs::create_directory(dst + "/depth");
+  bfs::create_directory(dst + "/rgb");
+  Frame frame;
+  for(size_t i = 0; i < sseq->size(); ++i) {
+    cout << i << " / " << sseq->size() << endl;
+    sseq->readFrame(i, &frame);
+    ostringstream oss;
+    
+    oss << dst << "/rgb/"
+        << setiosflags(ios::fixed) << setprecision(6) << setfill('0') << setw(16)
+        << sseq->timestamps_[i] << ".png";
+    cv::imwrite(oss.str(), frame.img_);
+
+    cv::Mat_<ushort> depth(cv::Size(frame.depth_->cols(), frame.depth_->rows()), 0);
+    for(int y = 0; y < depth.rows; ++y) {
+      for(int x = 0; x < depth.cols; ++x) {
+        depth(y, x) = frame.depth_->coeffRef(y, x);
+      }
+    }
+    oss.str("");
+    oss << dst << "/depth/"
+        << setiosflags(ios::fixed) << setprecision(6) << setfill('0') << setw(16)
+        << sseq->timestamps_[i] << ".png";
+    cv::imwrite(oss.str(), depth);
+    cout << "Saved to " << oss.str() << endl;
+  }
+
+  return 0;
+}
